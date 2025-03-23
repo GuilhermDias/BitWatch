4ec21304580a1483be96624092f13b8e6622ddaa
Guilherme Dias <guilhermediasthomaz@hotmail.com>
2025-03-23 19:43:52 -0300

    refactor: Adicionando anotação para validar email e padrão do documento

--- bitWatch/src/main/java/com/guilherme/bitWatch/domain/user/User.java
+++ bitWatch/src/main/java/com/guilherme/bitWatch/domain/user/User.java
@@ -1,6 +1,8 @@
 package com.guilherme.bitWatch.domain.user;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +21,11 @@ public class User {
     private Long id;
     private String name;
     private String lastName;
+
+    @Email
     private String email;
+
+    @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
     private String document;
     private String password;
 
